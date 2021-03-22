57e17c2d74a6d11f7662310dd898ee8c18403fad
DPK <dougkvamme@gmail.com>
2021-03-22 07:22:32 -0500

    initial commit

---
+++ build/precache-manifest.68ea8548ce193ee4d1967b5cc8bb0e94.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f579ce9f5ab84336db78bddad79e853f",
+    "url": "/trydo/index.html"
+  },
+  {
+    "revision": "0478cef6790aa64df667",
+    "url": "/trydo/static/css/2.a44debfe.chunk.css"
+  },
+  {
+    "revision": "408c7593137737e325a7",
+    "url": "/trydo/static/css/main.22f2c18a.chunk.css"
+  },
+  {
+    "revision": "0478cef6790aa64df667",
+    "url": "/trydo/static/js/2.c12cebc5.chunk.js"
+  },
+  {
+    "revision": "408c7593137737e325a7",
+    "url": "/trydo/static/js/main.bf032dc8.chunk.js"
+  },
+  {
+    "revision": "1e4299c178b0ee518a5b",
+    "url": "/trydo/static/js/runtime~main.a657da8a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/trydo/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/trydo/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/trydo/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/trydo/static/media/slick.f97e3bbf.svg"
+  }
+]);
